6ac121e88bc949e8b29e24f2472a582dba156655
書瑋 <126168463+WaltLiou@users.noreply.github.com>
2023-02-23 14:55:39 +0000

    Added html banner

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 # https://www.youtube.com/watch?v=yBDHkveJUf4&t=11097s
 print("Hello World")
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "Hello Worldsss"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+
+# htmldog.com   <=HTML語法
+# excalidraw.com  <=白板
+# unsplash.com    <=找圖片
+# lorem text generator  <=文本生成器
